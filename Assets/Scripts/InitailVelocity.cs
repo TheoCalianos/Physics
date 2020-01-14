41a7640baa087967ce20da80553b78f30dfdb825
Theo Calianos <43078567+TheoCalianos@users.noreply.github.com>
2020-01-14 12:14:21 -0500

    Magnus effect

---
+++ Assets/Scripts/InitailVelocity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitailVelocity : MonoBehaviour
+{
+    public Vector3 intialVelocity;
+    public Vector3 intialw;
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody> ();
+        rigidBody.velocity = intialVelocity;
+        rigidBody.angularVelocity = intialw;
+    }
+}
